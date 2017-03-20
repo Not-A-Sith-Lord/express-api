3810cdaf36e2d7a61b5222180804776e81705fef
Not-A-Sith-Lord <arieldeschapell@gmail.com>
2017-03-20 11:31:01 -0400

    more working routes

--- routes/phones-api.js
+++ routes/phones-api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Phone = require('../models/phone-model');
+const mongoose = require('mongoose');
 
 router.get('/phones', (req, res, next) => {
   Phone.find((err, phonesList) => {
@@ -11,7 +12,9 @@ router.get('/phones', (req, res, next) => {
 
     res.json(phonesList);
   });
-})
+});
+
+
 router.post("/phones", (req, res, next) => {
 
   const thePhone = new Phone ({
@@ -35,6 +38,60 @@ router.post("/phones", (req, res, next) => {
 
 });
 
+router.get("/phones/:id", (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400)
+      .json({ message: "specified id is not valid"});
+      return;
+  }
+
+  Phone.findById(req.params.id, (err, thePhone) => {
+    if (err) {
+      res.json(err);
+      return;
+    }
+
+    res.json(thePhone);
+  });
+});
+
+router.put('/phones/:id', (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400)
+      .json({ message: 'Specified id is not valid'});
+    return;
+  }
+
+  const updates = {
+    brand: req.body.brand,
+    name: req.body.name,
+    specs: req.body.specs,
+    image: req.body.image
+  };
+
+  Phone.findByIdAndUpdate(req.params.id, updates, (err) => {
+    if (err) {
+      res.json(err);
+      return;
+    }
+
+    res.json({
+      message: "Phone updated sucessfully"
+    });
+  });
+});
+
+router.delete('/phones/:id', (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400)
+      .json({ message: "Specified id is not valid"});
+    return;
+  }
+
+  res.json({
+    message: "Phone has been removed"
+  });
+});
 
 
 module.exports = router;
